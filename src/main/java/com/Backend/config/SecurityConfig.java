aa7b0d2c7dc39996f3be6049eb258760b6bcfc08
bpaul006 <112741012+bpaul006@users.noreply.github.com>
2024-11-11 21:08:09 -0500

    Fixing User Controller

--- src/main/java/com/Backend/config/SecurityConfig.java
+++ src/main/java/com/Backend/config/SecurityConfig.java
@@ -16,7 +16,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 
-@Configuration
+@Component
 public class SecurityConfig {
 
     private final UserService userService;
