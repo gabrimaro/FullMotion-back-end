35813421dad1470f5a61f293cde46f416cb1b49a
bpaul006 <112741012+bpaul006@users.noreply.github.com>
2024-12-02 20:44:24 -0500

    Revert "Fixing User entity"

--- src/main/java/com/Backend/dto/UserResponseDTO.java
+++ src/main/java/com/Backend/dto/UserResponseDTO.java
@@ -15,13 +15,15 @@ public class UserResponseDTO {
     private String email;
     private String prefix;
     private String suffix;
+    private List<Patient> patients;
 
-    public UserResponseDTO(String username, String firstName, String lastName, String email, String prefix, String suffix) {
+    public UserResponseDTO(String username, String firstName, String lastName, String email, String prefix, String suffix, List<Patient> patients) {
         this.username = username;
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
         this.prefix = prefix;
         this.suffix = suffix;
+        this.patients = patients;
     }
 }
